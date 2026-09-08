ee8baed281ebe2be1d08a3664e6fad1a895c8f04
Aayushi Agarwal <aaayushi373@gmail.com>
2026-09-09 01:12:43 +0530

    Updated responsive Wanderly website

---
+++ script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+const close = document.querySelector(".close");
+
+hamburger.addEventListener("click", function () {
+    mobileMenu.classList.add("active");
+});
+
+close.addEventListener("click", function () {
+    mobileMenu.classList.remove("active");
+});
+const email = document.querySelector("#email");
+const subscribe = document.querySelector("#subscribe");
+const message = document.querySelector("#message");
+
+subscribe.addEventListener("click", function () {
+
+    if (email.value.includes("@")) {
+        message.textContent = "Successfully subscribed!";
+    } else {
+        message.textContent = "Please enter a valid email!";
+    }
+
+});
