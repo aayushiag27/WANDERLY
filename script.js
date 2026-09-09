2ffc42cc9694e6418f5e21168fb50059e729e37d
Aayushi Agarwal <aaayushi373@gmail.com>
2026-09-09 13:15:00 +0530

    Added responsive design and JavaScript functionality

--- script.js
+++ script.js
@@ -9,16 +9,15 @@ hamburger.addEventListener("click", function () {
 close.addEventListener("click", function () {
     mobileMenu.classList.remove("active");
 });
+
 const email = document.querySelector("#email");
 const subscribe = document.querySelector("#subscribe");
 const message = document.querySelector("#message");
 
 subscribe.addEventListener("click", function () {
-
     if (email.value.includes("@")) {
         message.textContent = "Successfully subscribed!";
     } else {
         message.textContent = "Please enter a valid email!";
     }
-
 });
